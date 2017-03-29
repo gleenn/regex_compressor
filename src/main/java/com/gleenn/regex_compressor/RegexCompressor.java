45ab098a0760a1f48ec90d06941b5b9cc8932e4b
Glenn Jahnke <gleenn@gmail.com>
2017-03-29 12:41:13 -0700

    doing a little better on parenthasizing based on terminality and having children

--- src/main/java/com/gleenn/regex_compressor/RegexCompressor.java
+++ src/main/java/com/gleenn/regex_compressor/RegexCompressor.java
@@ -15,12 +15,9 @@ public class RegexCompressor {
     }
 
     public static void buildRegex(Trie trie, StringBuilder result) {
-        buildRegex(trie, false, result);
-    }
-
-    public static void buildRegex(Trie trie, boolean inGroup, StringBuilder result) {
         if(trie == null) throw new RuntimeException("Trie cannot be null");
         Character character = trie.getCharacter();
+        System.out.println("My name is " + character + " and I am " + (trie.isTerminal() ? "" : "not") + " terminal");
         if(character != null) {
             result.append(character);
         }
@@ -31,20 +28,20 @@ public class RegexCompressor {
             return;
         }
 
-        if(childrenTries.size() == 1) {
+        if(!trie.isTerminal() || childrenTries.size() > 1) {
             for(Trie child : childrenTries.values()) {
-                buildRegex(child, false, result);
+                buildRegex(child, result);
             }
         } else {
             result.append("(?:");
             for(Trie child : childrenTries.values()) {
-                buildRegex(child, inGroup, result);
+                buildRegex(child, result);
                 result.append("|");
             }
             result.deleteCharAt(result.length()-1); // remove extra "|"
             result.append(")");
         }
-        if(!trie.isTerminal()) {
+        if(trie.isTerminal()) {
             result.append("?");
         }
     }
