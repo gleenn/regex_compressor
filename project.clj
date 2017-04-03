c6316e40cc3ab97f42f6b0c6a649966d2914ec1c
Glenn Jahnke <gleenn@gmail.com>
2017-04-03 11:20:36 -0700

    WIP trying to add lein junit plugin so we can build via leiningen on circle to avoid crappy github mvn site plugin which doesn't work on circleci - use lein to run tests instead; added intentionally failing test

--- project.clj
+++ project.clj
@@ -2,10 +2,16 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[frak "0.1.6"]
-                                  [criterium "0.4.4"]]}}
-  :plugins [[lein-virgil "0.1.6"]]
-  :java-source-paths ["src/main/java"]
+  :profiles {:dev        {:dependencies [[frak "0.1.6"]
+                                         [criterium "0.4.4"]
+                                         [junit/junit "4.12"]
+                                         [org.hamcrest/hamcrest-all "1.3"]
+                                         [org.testng/testng "RELEASE"]]}
+             }
+  :plugins [[lein-virgil "0.1.6"]
+            [lein-junit "1.1.8"]]
+  :java-source-paths ["src/main/java" "src/test/java"]
+  :junit ["src/test/java"]
   :jvm-opts ["-Xmx2g"])
