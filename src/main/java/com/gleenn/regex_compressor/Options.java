ab56668c1d826dc770f917ab36f09b81600f4ba7
Glenn Jahnke <gleenn@gmail.com>
2017-04-10 19:21:42 -0700

    start options class which will hold parameters for building the compressed regex

---
+++ src/main/java/com/gleenn/regex_compressor/Options.java
@@ -0,0 +1,61 @@
+package com.gleenn.regex_compressor;
+
+public final class Options {
+    private boolean caseSensitive = true;
+    private String prefix = "";
+    private String suffix = "";
+
+    public Options() { }
+
+    private Options(Options options) {
+        caseSensitive = options.caseSensitive;
+        prefix = options.prefix;
+        suffix = options.suffix;
+    }
+
+    public static Options defaultOptions() {
+        return new Options();
+    }
+
+    public Options caseSensitive(final boolean caseSensitive) {
+        Options options = new Options(this);
+        options.caseSensitive = caseSensitive;
+        return options;
+    }
+
+    public Options withWordBoundaries() {
+        Options options = new Options(this);
+        options.prefix = options.suffix = "\\b";
+        return options;
+    }
+
+    public Options withWordBoundaries(final String ix) {
+        Options options = new Options(this);
+        options.prefix = options.suffix = ix;
+        return options;
+    }
+
+    public Options prefix(final String prefix) {
+        Options options = new Options(this);
+        options.prefix = prefix;
+        return options;
+    }
+
+    public Options suffix(final String suffix) {
+        Options options = new Options(this);
+        options.suffix = suffix;
+        return options;
+    }
+
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+}
---
+++ src/test/java/com/gleenn/OptionsTest.java
@@ -0,0 +1,37 @@
+package com.gleenn;
+
+import com.gleenn.regex_compressor.Options;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.testng.annotations.Test;
+
+public class OptionsTest {
+    @Test
+    public void caseSensitive() {
+        assertThat(Options.defaultOptions().isCaseSensitive(), is(true));
+
+        assertThat(Options.defaultOptions().caseSensitive(false).isCaseSensitive(), is(false));
+        assertThat(Options.defaultOptions().caseSensitive(true).isCaseSensitive(), is(true));
+    }
+
+    @Test
+    public void prefix() {
+        assertThat(Options.defaultOptions().getPrefix(), is(""));
+        assertThat(Options.defaultOptions().prefix("foobar").getPrefix(), is("foobar"));
+    }
+
+    @Test
+    public void suffix() {
+        assertThat(Options.defaultOptions().getSuffix(), is(""));
+        assertThat(Options.defaultOptions().suffix("foobar").getSuffix(), is("foobar"));
+    }
+
+    @Test
+    public void withWordBoundaries() {
+        assertThat(Options.defaultOptions().withWordBoundaries().getPrefix(), is("\\b"));
+        assertThat(Options.defaultOptions().withWordBoundaries().getSuffix(), is("\\b"));
+
+        assertThat(Options.defaultOptions().withWordBoundaries("foobar").getPrefix(), is("foobar"));
+        assertThat(Options.defaultOptions().withWordBoundaries("foobar").getSuffix(), is("foobar"));
+    }
+}
