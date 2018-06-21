78d1a0cae08ea2243f558d8dade1b880a10c5a38
Glenn Jahnke <gleenn@gmail.com>
2018-06-20 20:09:52 -0700

    remove unused import

--- src/test/java/com/gleenn/SimpleTrieTest.java
+++ src/test/java/com/gleenn/SimpleTrieTest.java
@@ -11,8 +11,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
-import java.util.Collections;
-
 public class SimpleTrieTest {
     @Test
     public void addWordTest_setsTerminalPropertyCorrectly() {
