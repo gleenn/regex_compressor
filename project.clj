659032c35791e7f4c4d18bfdd99fc0b83c4d4f7e
Glenn Jahnke <gleenn@gmail.com>
2018-06-20 20:12:40 -0700

    Add description to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject regex-compressor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Compresses regexes!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
